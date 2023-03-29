2b4553e39d21a49f294739f742a1280103a0570b
habibullahturkmen <habibullah_turkmen@yahoo.com>
2023-03-30 00:21:56 +0430

    profile component done

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,44 @@
+import { Avatar, Box, Typography } from "@mui/material"
+import React, { FC, ReactElement } from "react"
+import PropTypes from "prop-types"
+
+interface IProfile {
+  name: string
+}
+
+const Profile: FC<IProfile> = (props): ReactElement => {
+  const { name = "Habib" } = props
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Avatar
+        sx={{
+          width: "96px",
+          height: "96px",
+          backgroundColor: "primary.main",
+          marginBottom: "16px",
+        }}
+      >
+        <Typography variant="h4" color="text.primary">
+          {name[0]}
+        </Typography>
+      </Avatar>
+      <Typography variant="h6" color="text.primary">
+        {`Welcome, ${name}`}
+      </Typography>
+      <Typography variant="body1" color="text.primary">
+        This is your personal tasks manager
+      </Typography>
+    </Box>
+  )
+}
+
+export default Profile
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+}
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactElement } from "react"
 import { Grid } from "@mui/material"
+import Profile from "../profile/profile"
 
 const Sidebar: FC = (): ReactElement => {
   return (
@@ -19,7 +20,7 @@ const Sidebar: FC = (): ReactElement => {
         alignItems: "center",
       }}
     >
-      <h2>Sidebar Area</h2>
+      <Profile name="Habibullah" />
     </Grid>
   )
 }
